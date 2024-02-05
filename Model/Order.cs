08f48586bbb33c3a76ea224b3044f0cd25ccc545
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-05 12:12:40 +0530

    Upload Order.cs

---
+++ Model/Order.cs
@@ -0,0 +1,63 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Testing_Git.Model
+{
+    /// <summary> 
+    /// Represents a order entity with essential details
+    /// </summary>
+    public class Order
+    {
+        /// <summary>
+        /// Primary key for the Order 
+        /// </summary>
+        [Key]
+        public Guid OrderID { get; set; }
+        /// <summary>
+        /// Foreign key referencing the Customer to which the Order belongs 
+        /// </summary>
+        public Guid CustomerId { get; set; }
+
+        /// <summary>
+        /// Navigation property representing the associated Customer
+        /// </summary>
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+        /// <summary>
+        /// TotalAmount of the Order 
+        /// </summary>
+        public string TotalAmount { get; set; }
+        /// <summary>
+        /// Foreign key referencing the OrderStatus to which the Order belongs 
+        /// </summary>
+        public Guid OrderStatusId { get; set; }
+
+        /// <summary>
+        /// Navigation property representing the associated OrderStatus
+        /// </summary>
+        [ForeignKey("OrderStatusId")]
+        public OrderStatus OrderStatus { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<OrderLine> OrderLines { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<OrderLine1> OrderLine1s { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<OrderLine11> OrderLine11s { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<OrderLine10> OrderLine10s { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<OrderLine15> OrderLine15s { get; set; }
+    }
+}
