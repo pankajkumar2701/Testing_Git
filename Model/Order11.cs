831d24e06590f1512f65ab12a4876042dda95b10
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-05 12:12:45 +0530

    Upload Order11.cs

---
+++ Model/Order11.cs
@@ -0,0 +1,43 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Testing_Git.Model
+{
+    /// <summary> 
+    /// Represents a order11 entity with essential details
+    /// </summary>
+    public class Order11
+    {
+        /// <summary>
+        /// Primary key for the Order11 
+        /// </summary>
+        [Key]
+        public Guid OrderID { get; set; }
+        /// <summary>
+        /// Foreign key referencing the Customer to which the Order11 belongs 
+        /// </summary>
+        public Guid CustomerId { get; set; }
+
+        /// <summary>
+        /// Navigation property representing the associated Customer
+        /// </summary>
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+        /// <summary>
+        /// TotalAmount of the Order11 
+        /// </summary>
+        public string TotalAmount { get; set; }
+        /// <summary>
+        /// Foreign key referencing the OrderStatus to which the Order11 belongs 
+        /// </summary>
+        public Guid OrderStatusId { get; set; }
+
+        /// <summary>
+        /// Navigation property representing the associated OrderStatus
+        /// </summary>
+        [ForeignKey("OrderStatusId")]
+        public OrderStatus OrderStatus { get; set; }
+    }
+}
