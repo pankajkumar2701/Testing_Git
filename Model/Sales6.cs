0f16e1ea18c8dc4d342491aed2d643f2053f901b
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-05 13:10:55 +0530

    Upload Sales6.cs

---
+++ Model/Sales6.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Testing_Git.Model
+{
+    /// <summary> 
+    /// Represents a sales6 entity with essential details
+    /// </summary>
+    public class Sales6
+    {
+        /// <summary>
+        /// Primary key for the Sales6 
+        /// </summary>
+        [Key]
+        public Guid SalesId { get; set; }
+        /// <summary>
+        /// Name of the Sales6 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Foreign key referencing the Product to which the Sales6 belongs 
+        /// </summary>
+        public Guid ProductId { get; set; }
+
+        /// <summary>
+        /// Navigation property representing the associated Product
+        /// </summary>
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        /// <summary>
+        /// CustomerId of the Sales6 
+        /// </summary>
+        public string CustomerId { get; set; }
+    }
+}
