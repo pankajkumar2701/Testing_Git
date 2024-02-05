280e66257dedb84edd9e5fd8f249d4db736c2bbc
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-05 12:12:24 +0530

    Upload FilterCriteria.cs

---
+++ Filter/FilterCriteria.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Testing_Git.Filter
+{
+    public class FilterCriteria
+    {
+        public string PropertyName { get; set; }
+        public string Operator { get; set; }
+        public string Value { get; set; }
+    }
+}
