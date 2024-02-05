11be8d2ae15c554ff1bc4f508cd0aca53fec2c14
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-05 12:40:08 +0530

    Upload Testing_GitContext.cs

--- Data/Testing_GitContext.cs
+++ Data/Testing_GitContext.cs
@@ -32,7 +32,7 @@ namespace Testing_Git.Data
             modelBuilder.Entity<Order11>().HasKey(a => a.OrderID);
             modelBuilder.Entity<OrderLine11>().HasKey(a => a.OrderLineId);
             modelBuilder.Entity<Sales2>().HasKey(a => a.SalesId);
-            modelBuilder.Entity<OrderStatus3>().HasKey(a => a.OrderStatusId);
+            modelBuilder.Entity<OrderStatus35>().HasKey(a => a.OrderStatusId);
             modelBuilder.Entity<Product11>().HasKey(a => a.ProductId);
             modelBuilder.Entity<Employee>().HasKey(a => a.EmployeeId);
             modelBuilder.Entity<Employee1>().HasKey(a => a.EmployeeId1);
@@ -44,6 +44,35 @@ namespace Testing_Git.Data
             modelBuilder.Entity<Order15>().HasKey(a => a.OrderID);
             modelBuilder.Entity<OrderLine15>().HasKey(a => a.OrderLineId);
             modelBuilder.Entity<Product15>().HasKey(a => a.ProductId);
+            modelBuilder.Entity<Customer52>().HasKey(a => a.CustomerId);
+            modelBuilder.Entity<Order74>().HasKey(a => a.OrderID);
+            modelBuilder.Entity<OrderLine74>().HasKey(a => a.OrderLineId);
+            modelBuilder.Entity<Product74>().HasKey(a => a.ProductId);
+            modelBuilder.Entity<OrderStatus15>().HasKey(a => a.OrderStatusId);
+            modelBuilder.Entity<Country55>().HasKey(a => a.CountryId);
+            modelBuilder.Entity<Sales55>().HasKey(a => a.SalesId);
+            modelBuilder.Entity<OrderStatus24>().HasKey(a => a.OrderStatusId);
+            modelBuilder.Entity<Product18>().HasKey(a => a.ProductId);
+            modelBuilder.Entity<OrderStatus17>().HasKey(a => a.OrderStatusId);
+            modelBuilder.Entity<Country17>().HasKey(a => a.CountryId);
+            modelBuilder.Entity<Sales17>().HasKey(a => a.SalesId);
+            modelBuilder.Entity<Customer144>().HasKey(a => a.CustomerId);
+            modelBuilder.Entity<Order114>().HasKey(a => a.OrderID);
+            modelBuilder.Entity<OrderLine14>().HasKey(a => a.OrderLineId);
+            modelBuilder.Entity<OrderLine114>().HasKey(a => a.OrderLineId);
+            modelBuilder.Entity<Sales21>().HasKey(a => a.SalesId);
+            modelBuilder.Entity<OrderStatus3>().HasKey(a => a.OrderStatusId);
+            modelBuilder.Entity<Product114>().HasKey(a => a.ProductId);
+            modelBuilder.Entity<Employee14>().HasKey(a => a.EmployeeId);
+            modelBuilder.Entity<Employee11>().HasKey(a => a.EmployeeId1);
+            modelBuilder.Entity<Product131>().HasKey(a => a.ProductId);
+            modelBuilder.Entity<Employee31>().HasKey(a => a.EmployeeId);
+            modelBuilder.Entity<Order101>().HasKey(a => a.OrderID);
+            modelBuilder.Entity<OrderLine101>().HasKey(a => a.OrderLineId);
+            modelBuilder.Entity<Product101>().HasKey(a => a.ProductId);
+            modelBuilder.Entity<Order151>().HasKey(a => a.OrderID);
+            modelBuilder.Entity<OrderLine151>().HasKey(a => a.OrderLineId);
+            modelBuilder.Entity<Product151>().HasKey(a => a.ProductId);
             modelBuilder.Entity<Order>().HasOne(a => a.Customer).WithMany(b => b.Orders).HasForeignKey(c => c.CustomerId);
             modelBuilder.Entity<Order>().HasOne(a => a.OrderStatus).WithMany(b => b.Orders).HasForeignKey(c => c.OrderStatusId);
             modelBuilder.Entity<OrderLine>().HasOne(a => a.Order).WithMany(b => b.OrderLines).HasForeignKey(c => c.OrderId);
@@ -67,6 +96,27 @@ namespace Testing_Git.Data
             modelBuilder.Entity<Order15>().HasOne(a => a.OrderStatus).WithMany(b => b.Order15s).HasForeignKey(c => c.OrderStatusId);
             modelBuilder.Entity<OrderLine15>().HasOne(a => a.Order).WithMany(b => b.OrderLine15s).HasForeignKey(c => c.OrderId);
             modelBuilder.Entity<OrderLine15>().HasOne(a => a.Product).WithMany(b => b.OrderLine15s).HasForeignKey(c => c.ProductId);
+            modelBuilder.Entity<Order74>().HasOne(a => a.Customer).WithMany(b => b.Order74s).HasForeignKey(c => c.CustomerId);
+            modelBuilder.Entity<Order74>().HasOne(a => a.OrderStatus).WithMany(b => b.Order74s).HasForeignKey(c => c.OrderStatusId);
+            modelBuilder.Entity<OrderLine74>().HasOne(a => a.Order).WithMany(b => b.OrderLine74s).HasForeignKey(c => c.OrderId);
+            modelBuilder.Entity<OrderLine74>().HasOne(a => a.Product).WithMany(b => b.OrderLine74s).HasForeignKey(c => c.ProductId);
+            modelBuilder.Entity<Sales55>().HasOne(a => a.Product).WithMany(b => b.Sales55s).HasForeignKey(c => c.ProductId);
+            modelBuilder.Entity<Sales17>().HasOne(a => a.Product).WithMany(b => b.Sales17s).HasForeignKey(c => c.ProductId);
+            modelBuilder.Entity<Order114>().HasOne(a => a.Customer).WithMany(b => b.Order114s).HasForeignKey(c => c.CustomerId);
+            modelBuilder.Entity<Order114>().HasOne(a => a.OrderStatus).WithMany(b => b.Order114s).HasForeignKey(c => c.OrderStatusId);
+            modelBuilder.Entity<OrderLine14>().HasOne(a => a.Order).WithMany(b => b.OrderLine14s).HasForeignKey(c => c.OrderId);
+            modelBuilder.Entity<OrderLine14>().HasOne(a => a.Product).WithMany(b => b.OrderLine14s).HasForeignKey(c => c.ProductId);
+            modelBuilder.Entity<OrderLine114>().HasOne(a => a.Order).WithMany(b => b.OrderLine114s).HasForeignKey(c => c.OrderId);
+            modelBuilder.Entity<OrderLine114>().HasOne(a => a.Product).WithMany(b => b.OrderLine114s).HasForeignKey(c => c.ProductId);
+            modelBuilder.Entity<Sales21>().HasOne(a => a.Product).WithMany(b => b.Sales21s).HasForeignKey(c => c.ProductId);
+            modelBuilder.Entity<Order101>().HasOne(a => a.Customer).WithMany(b => b.Order101s).HasForeignKey(c => c.CustomerId);
+            modelBuilder.Entity<Order101>().HasOne(a => a.OrderStatus).WithMany(b => b.Order101s).HasForeignKey(c => c.OrderStatusId);
+            modelBuilder.Entity<OrderLine101>().HasOne(a => a.Order).WithMany(b => b.OrderLine101s).HasForeignKey(c => c.OrderId);
+            modelBuilder.Entity<OrderLine101>().HasOne(a => a.Product).WithMany(b => b.OrderLine101s).HasForeignKey(c => c.ProductId);
+            modelBuilder.Entity<Order151>().HasOne(a => a.Customer).WithMany(b => b.Order151s).HasForeignKey(c => c.CustomerId);
+            modelBuilder.Entity<Order151>().HasOne(a => a.OrderStatus).WithMany(b => b.Order151s).HasForeignKey(c => c.OrderStatusId);
+            modelBuilder.Entity<OrderLine151>().HasOne(a => a.Order).WithMany(b => b.OrderLine151s).HasForeignKey(c => c.OrderId);
+            modelBuilder.Entity<OrderLine151>().HasOne(a => a.Product).WithMany(b => b.OrderLine151s).HasForeignKey(c => c.ProductId);
         }
 
         public DbSet<Customer> Customer { get; set; }
@@ -87,7 +137,7 @@ namespace Testing_Git.Data
         public DbSet<Order11> Order11 { get; set; }
         public DbSet<OrderLine11> OrderLine11 { get; set; }
         public DbSet<Sales2> Sales2 { get; set; }
-        public DbSet<OrderStatus3> OrderStatus3 { get; set; }
+        public DbSet<OrderStatus35> OrderStatus35 { get; set; }
         public DbSet<Product11> Product11 { get; set; }
         public DbSet<Employee> Employee { get; set; }
         public DbSet<Employee1> Employee1 { get; set; }
@@ -99,5 +149,34 @@ namespace Testing_Git.Data
         public DbSet<Order15> Order15 { get; set; }
         public DbSet<OrderLine15> OrderLine15 { get; set; }
         public DbSet<Product15> Product15 { get; set; }
+        public DbSet<Customer52> Customer52 { get; set; }
+        public DbSet<Order74> Order74 { get; set; }
+        public DbSet<OrderLine74> OrderLine74 { get; set; }
+        public DbSet<Product74> Product74 { get; set; }
+        public DbSet<OrderStatus15> OrderStatus15 { get; set; }
+        public DbSet<Country55> Country55 { get; set; }
+        public DbSet<Sales55> Sales55 { get; set; }
+        public DbSet<OrderStatus24> OrderStatus24 { get; set; }
+        public DbSet<Product18> Product18 { get; set; }
+        public DbSet<OrderStatus17> OrderStatus17 { get; set; }
+        public DbSet<Country17> Country17 { get; set; }
+        public DbSet<Sales17> Sales17 { get; set; }
+        public DbSet<Customer144> Customer144 { get; set; }
+        public DbSet<Order114> Order114 { get; set; }
+        public DbSet<OrderLine14> OrderLine14 { get; set; }
+        public DbSet<OrderLine114> OrderLine114 { get; set; }
+        public DbSet<Sales21> Sales21 { get; set; }
+        public DbSet<OrderStatus3> OrderStatus3 { get; set; }
+        public DbSet<Product114> Product114 { get; set; }
+        public DbSet<Employee14> Employee14 { get; set; }
+        public DbSet<Employee11> Employee11 { get; set; }
+        public DbSet<Product131> Product131 { get; set; }
+        public DbSet<Employee31> Employee31 { get; set; }
+        public DbSet<Order101> Order101 { get; set; }
+        public DbSet<OrderLine101> OrderLine101 { get; set; }
+        public DbSet<Product101> Product101 { get; set; }
+        public DbSet<Order151> Order151 { get; set; }
+        public DbSet<OrderLine151> OrderLine151 { get; set; }
+        public DbSet<Product151> Product151 { get; set; }
     }
 }
