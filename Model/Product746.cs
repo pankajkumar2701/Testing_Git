90b0ce9d377f166819e84699534c366be4c0c046
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-05 13:10:35 +0530

    Upload Product746.cs

---
+++ Model/Product746.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Testing_Git.Model
+{
+    /// <summary> 
+    /// Represents a product746 entity with essential details
+    /// </summary>
+    public class Product746
+    {
+        /// <summary>
+        /// Primary key for the Product746 
+        /// </summary>
+        [Key]
+        public Guid ProductId { get; set; }
+        /// <summary>
+        /// Name of the Product746 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Code of the Product746 
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// Price of the Product746 
+        /// </summary>
+        public string Price { get; set; }
+    }
+}
