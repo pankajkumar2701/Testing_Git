6ec1f0b11f3bd72092b2d73dd820ec8c6c75129b
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-05 12:12:57 +0530

    Upload OrderStatus.cs

---
+++ Model/OrderStatus.cs
@@ -0,0 +1,43 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Testing_Git.Model
+{
+    /// <summary> 
+    /// Represents a orderstatus entity with essential details
+    /// </summary>
+    public class OrderStatus
+    {
+        /// <summary>
+        /// Primary key for the OrderStatus 
+        /// </summary>
+        [Key]
+        public Guid OrderStatusId { get; set; }
+        /// <summary>
+        /// Name of the OrderStatus 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<Order> Orders { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<Order1> Order1s { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<Order11> Order11s { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<Order10> Order10s { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<Order15> Order15s { get; set; }
+    }
+}
