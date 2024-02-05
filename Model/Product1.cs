16e5d1edf6e3ba47f45fd7239af378e0a801007d
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-05 12:13:06 +0530

    Upload Product1.cs

---
+++ Model/Product1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Testing_Git.Model
+{
+    /// <summary> 
+    /// Represents a product1 entity with essential details
+    /// </summary>
+    public class Product1
+    {
+        /// <summary>
+        /// Primary key for the Product1 
+        /// </summary>
+        [Key]
+        public Guid ProductId { get; set; }
+        /// <summary>
+        /// Name of the Product1 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Code of the Product1 
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// Price of the Product1 
+        /// </summary>
+        public string Price { get; set; }
+    }
+}
