fc69adb9a11442b0bf737c47e67bcc0d241c5596
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-05 13:07:59 +0530

    Upload Customer526.cs

---
+++ Model/Customer526.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Testing_Git.Model
+{
+    /// <summary> 
+    /// Represents a customer526 entity with essential details
+    /// </summary>
+    public class Customer526
+    {
+        /// <summary>
+        /// Primary key for the Customer526 
+        /// </summary>
+        [Key]
+        public Guid CustomerId { get; set; }
+        /// <summary>
+        /// Name of the Customer526 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Address of the Customer526 
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// CountryName of the Customer526 
+        /// </summary>
+        public string CountryName { get; set; }
+    }
+}
