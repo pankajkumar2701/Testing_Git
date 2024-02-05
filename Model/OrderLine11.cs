c9101a28a72348ab50031f20ffc3c9cfe089544d
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-05 12:12:54 +0530

    Upload OrderLine11.cs

---
+++ Model/OrderLine11.cs
@@ -0,0 +1,43 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Testing_Git.Model
+{
+    /// <summary> 
+    /// Represents a orderline11 entity with essential details
+    /// </summary>
+    public class OrderLine11
+    {
+        /// <summary>
+        /// Primary key for the OrderLine11 
+        /// </summary>
+        [Key]
+        public Guid OrderLineId { get; set; }
+        /// <summary>
+        /// Foreign key referencing the Order to which the OrderLine11 belongs 
+        /// </summary>
+        public Guid OrderId { get; set; }
+
+        /// <summary>
+        /// Navigation property representing the associated Order
+        /// </summary>
+        [ForeignKey("OrderID")]
+        public Order Order { get; set; }
+        /// <summary>
+        /// Foreign key referencing the Product to which the OrderLine11 belongs 
+        /// </summary>
+        public Guid ProductId { get; set; }
+
+        /// <summary>
+        /// Navigation property representing the associated Product
+        /// </summary>
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        /// <summary>
+        /// Quantity of the OrderLine11 
+        /// </summary>
+        public string Quantity { get; set; }
+    }
+}
